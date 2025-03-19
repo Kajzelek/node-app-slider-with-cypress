13f08a37f07e764596dbd2d0cc2d8c71b893f6cb
Kajzelek <cygnar.patryk@gmail.com>
2025-03-19 20:56:08 +0100

    updated e2e

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,27 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Navigation Test', function () {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
+
+  it('Navigates to the next slide', function () {
+    cy.get('.swiper-slide-active').invoke('text').then((initialText) => {
+      cy.get('.swiper-button-next').click();
+      cy.wait(1000);
+      cy.get('.swiper-slide-active').invoke('text').should('not.equal', initialText);
+    });
+  });
+
+  it('Navigates to the previous slide', function () {
+    cy.get('.swiper-button-next').click();
+    cy.wait(1000);
+    cy.get('.swiper-slide-active').invoke('text').then((nextSlideText) => {
+      cy.get('.swiper-button-prev').click();
+      cy.wait(1000);
+      cy.get('.swiper-slide-active').invoke('text').should('not.equal', nextSlideText);
+    });
+  });
+});
